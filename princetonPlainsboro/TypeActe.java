44ff52d0521057021caa26ec7d6d36b633d9c19e
MaudLec <39367610+MaudLec@users.noreply.github.com>
2018-05-31 15:39:36 +0200

    Update TypeActe.java

--- princetonPlainsboro/TypeActe.java
+++ princetonPlainsboro/TypeActe.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Julie Dubois, Suzanne Guegaden, Maud Leclerc, Vaea Tesan
  */
 package princetonPlainsboro;
 
-/**
- *
- * @author GSuz-
- */
+
 public enum TypeActe {
         diagnostique, therapeutique;
     
