9fe0c4bc7b8a39233370369f4e8bb243f59b91cf
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-05-31 11:04:41 +0200

    Update Medecin.java

--- princetonPlainsboro/Medecin.java
+++ princetonPlainsboro/Medecin.java
@@ -1,30 +1,40 @@
 package princetonPlainsboro;
 
-class Medecin {
-    private String nom;
-    private String prenom;
-    private String specialite;
-    
-    public Medecin(String nom, String prenom, String specialite) {
-        this.nom = nom;
-        this.prenom = prenom;
+import java.util.ArrayList;
+
+class Medecin extends Personne {
+
+    private Specialite specialite;
+    private ArrayList<Integer> numtel;
+
+    public Medecin(String nom, String prenom, Sexe sexe, Date naissance, int numid, Specialite specialite, ArrayList<Integer> numtel) {
+        super(nom, prenom, sexe, naissance, numid);
         this.specialite = specialite;
-        }
+        if (numtel.size()==10) this.numtel=new ArrayList<Integer>();
+        
+    }
     
-    public String getSpecialite() { return specialite; }
-    
+    public String getNom() {
+        return this.nom;
+    }
+
+    public Specialite getSpecialite() {
+        return specialite;
+    }
+
     public String toString() {
         return "Dr " + prenom + " " + nom + ", " + specialite;
-        }
-    
+    }
+
     public boolean equals(Object o) {
         if (o instanceof Medecin) {
-            Medecin p = (Medecin)o;
+            Medecin p = (Medecin) o;
             return nom.equals(p.nom) && prenom.equals(p.prenom);
-            }
-        else
+        } else {
             return false;
-        }    
+        }
     }
+    
+}
 
 
