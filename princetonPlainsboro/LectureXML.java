052a6400389b8a50fecbff073ca3f7aba91f3ede
vaeat <39367652+vaeat@users.noreply.github.com>
2018-05-31 13:00:58 +0200

    Update LectureXML.java

--- princetonPlainsboro/LectureXML.java
+++ princetonPlainsboro/LectureXML.java
@@ -11,13 +11,12 @@ package princetonPlainsboro;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import princetonPlainsboro.Acte.Type;
-import princetonPlainsboro.Personne.Sexe;
 
 /**
  * Lecture d'un document XML et transformation en instances Java.
@@ -39,20 +38,13 @@ public class LectureXML {
         Date date = null;
         Medecin medecinCourant = null;
         Patient patientCourant= null;
-        Secretaire secretaireCourant = null;
-        ArrayList<Acte> actes = new ArrayList<Acte>();
+        List<Acte> actes = new Vector<Acte>();
         String donneesCourantes = "";
         String nomCourant = "";
         String prenomCourant = "";
-        Specialite specialiteCourante = Specialite.autre;
+        String specialiteCourante = "";
         Code codeCourant = null;
         int coefCourant = 0;
-        Type type=Type.diagnostique;
-        int numid=0;
-        ArrayList<Integer> numtel = new ArrayList<Integer>();
-        NumSecu numsecu = null;
-        String adresse=null;
-        Sexe sexe=null;
         
         // analyser le fichier par StAX
         try {
@@ -72,7 +64,7 @@ public class LectureXML {
                         break;
                     case XMLStreamConstants.END_ELEMENT:
                         if (parser.getLocalName().equals("acte")) {
-                            actes.add(new Acte(codeCourant, nomCourant, patientCourant, medecinCourant, date, type, coefCourant));
+                            actes.add(new Acte(codeCourant, coefCourant));
                         }                        
                         if (parser.getLocalName().equals("code")) {
                             codeCourant = getCode(donneesCourantes);
@@ -101,22 +93,21 @@ public class LectureXML {
                             // ajouter la fiche de soin au dossiers
                             dossierCourant.ajouterFiche(f);
                         }
-                        if (parser.getLocalName().equals("secretaire")) {
-                            secretaireCourant = new Secretaire(nomCourant, prenomCourant, sexe, date, numid);
-                        }
                         if (parser.getLocalName().equals("medecin")) {
-                            medecinCourant = new Medecin(nomCourant, prenomCourant, sexe, date, numid, specialiteCourante, numtel);
+                            medecinCourant = new Medecin(nomCourant, prenomCourant, specialiteCourante);
                         }
                         if (parser.getLocalName().equals("nom")) {
                             nomCourant = donneesCourantes;
                         }
                         if (parser.getLocalName().equals("patient")) {
-                            patientCourant = new Patient(nomCourant, prenomCourant, sexe, date, numid, numsecu, adresse);
+                            patientCourant = new Patient(nomCourant, prenomCourant);
                         }
                         if (parser.getLocalName().equals("prenom")) {
                             prenomCourant = donneesCourantes;
                         }
-
+                        if (parser.getLocalName().equals("specialite")) {
+                            specialiteCourante = donneesCourantes;
+                        }
                         break;
                     case XMLStreamConstants.CHARACTERS:
                         donneesCourantes = parser.getText();
@@ -138,28 +129,93 @@ public class LectureXML {
     }
     
     private static Code getCode(String code) {
+        
+         if (code.equals("C"))
+            return Code.C;
         if (code.equals("CS"))
             return Code.CS;
         if (code.equals("CSC"))
             return Code.CSC;
+        if (code.equals("APC"))
+            return Code.APC;
+        if (code.equals("APV"))
+            return Code.APV;
+        if (code.equals("APY"))
+            return Code.APY;
+        if (code.equals("AVY"))
+            return Code.AVY;
+        if (code.equals("APU"))
+            return Code.APU;
+        if (code.equals("CCP"))
+            return Code.CCP;
+        if (code.equals("U03"))
+            return Code.U03;
+        if (code.equals("U45"))
+            return Code.U45;
+        if (code.equals("COE"))
+            return Code.COE;
+        if (code.equals("CNPSY"))
+            return Code.CNPSY;
+        if (code.equals("CDE"))
+            return Code.CDE;
+        if (code.equals("V"))
+            return Code.V;
+        if (code.equals("VL"))
+            return Code.VL;
+        if (code.equals("VS"))
+            return Code.VS;
+        if (code.equals("VNPSY"))
+            return Code.VNPSY;
+        if (code.equals("K"))
+            return Code.K;
+        if (code.equals("KMB"))
+            return Code.KMB;
+        if (code.equals("Z"))
+            return Code.Z;
+        if (code.equals("ORT"))
+            return Code.ORT;
+        if (code.equals("TO"))
+            return Code.TO;
+        if (code.equals("SP"))
+            return Code.SP;
+        if (code.equals("SF"))
+            return Code.SF;
+        if (code.equals("FI"))
+            return Code.FI;
+        if (code.equals("AMS"))
+            return Code.AMS;
+        if (code.equals("AMK"))
+            return Code.AMK;
+        if (code.equals("AMC"))
+            return Code.AMC;
+        if (code.equals("AMI"))
+            return Code.AMI;
+        if (code.equals("AIS"))
+            return Code.AIS;
+        if (code.equals("DI"))
+            return Code.DI;
+        if (code.equals("AMP"))
+            return Code.AMP;
+        if (code.equals("POD"))
+            return Code.POD;
+        if (code.equals("AMO"))
+            return Code.AMO;
+        if (code.equals("AMY"))
+            return Code.AMY;
+        if (code.equals("VAC"))
+            return Code.VAC;
         if (code.equals("FP"))
             return Code.FP;
         if (code.equals("KC"))
             return Code.KC;
         if (code.equals("KE"))
             return Code.KE;
-        if (code.equals("K"))
-            return Code.K;
         if (code.equals("KFA"))
             return Code.KFA;
         if (code.equals("KFB"))
             return Code.KFB;
-        if (code.equals("ORT"))
-            return Code.ORT;
         if (code.equals("PRO"))
             return Code.PRO;
-        // probleme : code inconnu
         return null;            
     }
 }
-
