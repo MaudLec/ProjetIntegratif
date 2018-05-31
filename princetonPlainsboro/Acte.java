31e4e1ffe0e26beee429bd54af9ccb0b3989e3c6
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-05-31 15:10:59 +0200

    Update Acte.java

--- princetonPlainsboro/Acte.java
+++ princetonPlainsboro/Acte.java
@@ -1,19 +1,50 @@
 package princetonPlainsboro;
 
 class Acte {
+
     private Code code;
     private int coef;
-    
-    public Acte(Code code, int coef) {
+    private TypeActe type;
+    private String obs;
+
+    public Acte(Code code, int coef, TypeActe type, String obs) {
         this.code = code;
         this.coef = coef;
-        }
-    
+        this.type = type;
+        this.obs = obs;
+    }
+
+    public Code getCode() {
+        return code;
+    }
+
+    public int getCoef() {
+        return coef;
+    }
+
+    public TypeActe getType() {
+        return type;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
     public String toString() {
         return code.toString() + ", coefficient : " + coef;
-        }
-    
+    }
+
     public double cout() {
         return code.calculerCout(coef);
-        }
     }
+
+    public String coutActe() {
+        return "" + code.calculerCout(coef);
+    }
+
+//    public String imprimerActe() {
+//    }
+//
+//    public String factureActe() {
+//    }
+}
