d86fffd5824cc72d72f67161ed549089f755e06b
vaeat <39367652+vaeat@users.noreply.github.com>
2018-05-31 15:33:30 +0200

    Update SecretaireAdmin.java

--- princetonPlainsboro/SecretaireAdmin.java
+++ princetonPlainsboro/SecretaireAdmin.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package princetonPlainsboro;
 
-/**
- *
- * @author maudl
- */
 public class SecretaireAdmin {
-    
+
+    private String nom;
+    private String prenom;
+    private int id;
+    private String mdp;
+
+    public SecretaireAdmin(String nom, String prenom, int id, String mdp) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.id = id;
+        this.mdp = mdp;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
 }
