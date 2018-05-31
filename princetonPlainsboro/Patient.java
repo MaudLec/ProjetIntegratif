c44d2513a17735b1f6e444c801508418b6ddcdc7
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-05-31 11:05:22 +0200

    Update Patient.java

--- princetonPlainsboro/Patient.java
+++ princetonPlainsboro/Patient.java
@@ -1,25 +1,40 @@
 package princetonPlainsboro;
 
-class Patient {
-    private String nom;
-    private String prenom;
-    
-    public Patient(String nom, String prenom) {
-        this.nom = nom;
-        this.prenom = prenom;
-        }
-    
-    public String toString() {
-        return prenom + " " + nom;
-        }
-    
-    public boolean equals(Object o) {
-        if (o instanceof Patient) {
-            Patient p = (Patient)o;
-            return nom.equals(p.nom) && prenom.equals(p.prenom);
-            }
-        else
-            return false;
-        }    
+import java.util.ArrayList;
+
+class Patient extends Personne {
+
+    private NumSecu numsecu;
+    private String adresse;
+
+    public Patient(String nom, String prenom, Sexe sexe, Date naissance, int numid, NumSecu numsecu, String adresse) {
+        super(nom, prenom, sexe, naissance, numid);
+        this.numsecu = numsecu;
+        this.adresse = adresse;
+
     }
 
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String toString() {
+        String s = prenom + " " + nom + ", ";
+        if (sexe == Sexe.F) {
+            s += "femme";
+        } else {
+            s += "homme";
+        }
+        return s;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Patient) {
+            Patient p = (Patient) o;
+            return nom.equals(p.nom) && prenom.equals(p.prenom);
+        } else {
+            return false;
+        }
+    }
+
+}
