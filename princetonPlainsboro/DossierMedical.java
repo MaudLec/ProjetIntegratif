4f2b1118b51e58dbbf7d8fea1922a8e2d6e8b48a
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-06-03 15:00:11 +0200

    Update DossierMedical.java

--- princetonPlainsboro/DossierMedical.java
+++ princetonPlainsboro/DossierMedical.java
@@ -30,10 +30,6 @@ class DossierMedical {
     public ArrayList<FicheDeSoins> getFiches() {
         return fiches;
     }
-    
-    public FicheDeSoins getFiches(int i) {
-        return fiches.get(i);
-    }
 
     public void ajouterFiche(FicheDeSoins fiche) {
         fiches.add(fiche);
