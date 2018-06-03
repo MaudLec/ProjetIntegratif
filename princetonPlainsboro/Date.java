0f404ecbd522d48a7653884142b73f9bc13bb73c
MaudLec <39367610+MaudLec@users.noreply.github.com>
2018-06-03 12:51:52 +0200

    Update Date.java

--- princetonPlainsboro/Date.java
+++ princetonPlainsboro/Date.java
@@ -55,6 +55,33 @@ class Date implements Comparable {
         // ici on a forcement annee == d.annee et mois == d.mois :
         return jour - d.jour;
         }
- 
+ public String toXML() {
+        String s = "<date>" + annee;
+        if (mois < 10) {
+            s += "-0" + mois;
+        } else {
+            s += "-" + mois;
+        }
+        if (jour < 10) {
+            s += "-0" + jour;
+        } else {
+            s += "-" + jour;
+        }
+        if (minute != 0 && heure != 0) {
+            s+="T";
+            if (heure < 10) {
+                s += "0" + heure;
+            } else {
+                s += heure;
+            }
+             if (minute < 10) {
+                s += ":0" + minute;
+            } else {
+                s += ":" + minute;
+            }
+        }
+        s += "</date>";
+        return s;
+    }
     
     }
