de914fa38eaa57cd655817aa37f111d40b21420c
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-06-04 08:47:43 +0200

    Update ComparaisonFichesCouts.java

--- princetonPlainsboro/ComparaisonFichesCouts.java
+++ princetonPlainsboro/ComparaisonFichesCouts.java
@@ -1,6 +1,6 @@
 package princetonPlainsboro;
 
-class ComparaisonFichesCouts implements ComparaisonFiches {
+public class ComparaisonFichesCouts implements ComparaisonFiches {
     public int comparer(FicheDeSoins fiche1, FicheDeSoins fiche2) {
         Double c1 = new Double(fiche1.coutTotal());
         Double c2 = new Double(fiche2.coutTotal());
