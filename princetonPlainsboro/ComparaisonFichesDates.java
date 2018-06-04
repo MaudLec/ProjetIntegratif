be197a3f4200bd6896300ce5382d34ab3551519c
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-06-04 08:48:02 +0200

    Update ComparaisonFichesDates.java

--- princetonPlainsboro/ComparaisonFichesDates.java
+++ princetonPlainsboro/ComparaisonFichesDates.java
@@ -1,6 +1,6 @@
 package princetonPlainsboro;
 
-class ComparaisonFichesDates implements ComparaisonFiches {
+public class ComparaisonFichesDates implements ComparaisonFiches {
     public int comparer(FicheDeSoins fiche1, FicheDeSoins fiche2) {
         return fiche1.getDate().compareTo(fiche2.getDate());
         }
