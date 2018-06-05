96e07d7cf32eba5d42ba3886f31d98f1dbf761f5
MaudLec <39367610+MaudLec@users.noreply.github.com>
2018-06-05 16:13:18 +0200

    Delete ComparaisonFichesCouts.java

--- princetonPlainsboro/ComparaisonFichesCouts.java
+++
@@ -1,9 +0,0 @@
-package princetonPlainsboro;
-
-public class ComparaisonFichesCouts implements ComparaisonFiches {
-    public int comparer(FicheDeSoins fiche1, FicheDeSoins fiche2) {
-        Double c1 = new Double(fiche1.coutTotal());
-        Double c2 = new Double(fiche2.coutTotal());
-        return c1.compareTo(c2);
-        }
-    }
