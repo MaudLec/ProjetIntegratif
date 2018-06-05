760980fd1f5b4e84c99a95b76484b344d30e3e38
MaudLec <39367610+MaudLec@users.noreply.github.com>
2018-06-05 10:00:40 +0200

    Update IU.java

--- IU/IU.java
+++ IU/IU.java
@@ -2213,11 +2213,11 @@ public class IU extends javax.swing.JFrame {
             jTextField1.setText(ficheCourante.getMedecin().getNom());
             jTextField2.setText(ficheCourante.getMedecin().getPrenom());
             ChoixSpe1.setSelectedItem(ficheCourante.getMedecin().getSpecialite().toString());
-            //pour afficher les actes d'un patient
+             //pour afficher les actes d'un patient
             //maud
             DefaultListModel dlma = new DefaultListModel();
-            for (int ind = 0; ind < ficheCourante.getActes().size(); ind++) {
-                dlma.addElement(dossier.getFiche(ind).getActe(ind).toString());
+            for (int jnd = 0; jnd < ficheCourante.getActes().size(); jnd++) {
+                dlma.addElement(ficheCourante.getActe(jnd).toString());
             }
             jList5.setModel(dlma);
             jList7.setModel(dlma);
