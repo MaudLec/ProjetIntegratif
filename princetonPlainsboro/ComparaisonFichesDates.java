1698ab7cbad40085b78489e3f96a20234d09a0a0
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-06-05 16:11:42 +0200

    Delete ComparaisonFichesDates.java

--- princetonPlainsboro/ComparaisonFichesDates.java
+++
@@ -1,7 +0,0 @@
-package princetonPlainsboro;
-
-public class ComparaisonFichesDates implements ComparaisonFiches {
-    public int comparer(FicheDeSoins fiche1, FicheDeSoins fiche2) {
-        return fiche1.getDate().compareTo(fiche2.getDate());
-        }
-    }
