78dc881460102fc72d1c9965b162d26b668160b0
SuzanneGue <39792311+SuzanneGue@users.noreply.github.com>
2018-06-06 09:10:13 +0200

    Update Sexe.java

--- princetonPlainsboro/Sexe.java
+++ princetonPlainsboro/Sexe.java
@@ -4,6 +4,20 @@ public enum Sexe {
     M,
     F,
     Autre;
+    
+        public Sexe getSexe(String s) {
+        if (s.equals("F")) {
+            return Sexe.F;
+        }
+        if (s.equals("M")) {
+            return Sexe.M;
+        }
+        if (s.equals("Autre")) {
+            return Sexe.Autre;
+        }
+
+        return null;
+    }
 
     /* Affichage du sexe avec la balise XML */
     public String toXML() {
